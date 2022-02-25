a39795fc33e010179bfb86dccbf8da5f9a738069
foo.bar <foo.bar@lorem.ipsum>
2022-02-25 11:11:37 +0200

    Added batch files.

---
+++ http.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start cmd /c "mode con: cols=100 lines=30 & ServiceLocator\\bin\\Release\\net5.0\\ServiceLocator.exe"
---
+++ store.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start cmd /c "mode con: cols=100 lines=30 & MemoryStore\\bin\\Release\\net5.0\\MemoryStore.exe"
